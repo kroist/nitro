13aea800b1406f6346cfab526b74eef7b31751b6
Pepper Lebeck-Jobe <eljobe@gmail.com>
2024-05-17 19:37:39 +0200

    Try to get the deadlock thread to run.
    Still no luck, but sharing for help.

--- arbitrator/prover/src/machine.rs
+++ arbitrator/prover/src/machine.rs
@@ -53,6 +53,12 @@ use std::{
 #[cfg(feature = "deadlocks")]
 use std::{thread, time::Duration};
 
+#[cfg(feature = "deadlocks")]
+use std::sync::Once;
+
+#[cfg(feature = "deadlocks")]
+static START: Once = Once::new();
+
 use wasmer_types::FunctionIndex;
 use wasmparser::{DataKind, ElementItems, ElementKind, Operator, RefType, TableType};
 
@@ -994,8 +1000,6 @@ pub struct Machine {
     initial_hash: Bytes32,
     context: u64,
     debug_info: bool, // Not part of machine hash
-    #[cfg(feature = "deadlocks")]
-    deadlock_detection_running: bool,
 }
 
 type FrameStackHash = Bytes32;
@@ -1563,8 +1567,6 @@ impl Machine {
             initial_hash: Bytes32::default(),
             context: 0,
             debug_info,
-            #[cfg(feature = "deadlocks")]
-            deadlock_detection_running: false,
         };
         mach.initial_hash = mach.hash();
         Ok(mach)
@@ -1621,8 +1623,6 @@ impl Machine {
             initial_hash: Bytes32::default(),
             context: 0,
             debug_info: false,
-            #[cfg(feature = "deadlocks")]
-            deadlock_detection_running: false,
         };
         mach.initial_hash = mach.hash();
         Ok(mach)
@@ -1904,38 +1904,31 @@ impl Machine {
         self.steps
     }
 
-    #[cfg(feature = "deadlocks")]
-    fn enable_deadlock_detection(&mut self) {
-        if self.deadlock_detection_running {
-            return;
-        }
-        // Create a background thread which checks for deadlocks every 10s
-        thread::spawn(move || loop {
-            thread::sleep(Duration::from_secs(10));
-            let deadlocks = deadlock::check_deadlock();
-            if deadlocks.is_empty() {
-                println!("No deadlocks detected");
-                continue;
-            }
-
-            println!("{} deadlocks detected", deadlocks.len());
-            for (i, threads) in deadlocks.iter().enumerate() {
-                println!("Deadlock #{}", i);
-                for t in threads {
-                    println!("{:#?}", t.backtrace());
-                }
-            }
-        });
-        self.deadlock_detection_running = true;
-    }
-
     #[cfg(feature = "native")]
     pub fn step_n(&mut self, n: u64) -> Result<()> {
         if self.is_halted() {
             return Ok(());
         }
         #[cfg(feature = "deadlocks")]
-        self.enable_deadlock_detection();
+        START.call_once(|| {
+            thread::spawn(move || loop {
+                thread::sleep(Duration::from_secs(10));
+                let deadlocks = deadlock::check_deadlock();
+                if deadlocks.is_empty() {
+                    println!("No deadlocks detected");
+                    continue;
+                }
+
+                println!("{} deadlocks detected", deadlocks.len());
+                for (i, threads) in deadlocks.iter().enumerate() {
+                    println!("Deadlock #{}", i);
+                    for t in threads {
+                        println!("{:#?}", t.backtrace());
+                    }
+                }
+            });
+        });
+
         let (mut value_stack, mut frame_stack) = match self.thread_state {
             ThreadState::Main => (&mut self.value_stacks[0], &mut self.frame_stacks[0]),
             ThreadState::CoThread(_) => (
